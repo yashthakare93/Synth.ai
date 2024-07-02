0d9af2f73dab9714c1e40ce52523008fe28838ec
Yash Thakare <thakareyash74@gmail.com>
2024-07-02 22:01:54 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import 'semantic-ui-css/semantic.min.css';
 
 const App = () => {
   return (
-    <Router basename='/verce.ai'>
+    <Router basename='/verce-ai'>
       <Routes>
-        <Route exact path="/verce.ai" element={<Home />} />
+        <Route exact path="/verce-ai" element={<Home />} />
         <Route path="/main" element={<Main />} />
       </Routes>
     </Router>
