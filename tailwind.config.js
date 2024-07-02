14e835f5ba4800cdf1c34fdb58830fa4bdcfd463
Yash Thakare <thakareyash74@gmail.com>
2024-07-02 19:37:43 +0530

    confi updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  content: [
-  ],
+
   theme: {
     extend: {
       width:{
